6a93ab2e8a03e5c7edd7413e938e82cafc569e64
Aleksandr Volodin <cursedkun@gmail.com>
2021-04-22 23:43:43 +0300

    Add specification evaluator

--- Core/Specifications/ISpecification.cs
+++ Core/Specifications/ISpecification.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Core.Specifications
 {
-    interface ISpecification<T>
+    public interface ISpecification<T>
     {
         Expression<Func<T, bool>> Criteria { get; }
         List<Expression<Func<T, object>>> Includes { get; }
